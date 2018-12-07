ab8fbff91356c1c420da120537a9228d1160941c
Thales Andrade Pereira <thales_ap@hotmail.com>
2018-12-07 17:03:23 -0200

    CM - Adicionando execução firefox_headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,29 +15,52 @@ Capybara.app_host = 'https://automacaocombatista.herokuapp.com/'
 #Retirando esse register e o default driver eu recebo erro de "rack test requires a rack application"
 
 
+=begin 
 begin Capybara.register_driver :chrome do |app|
     Capybara::Selenium::Driver.new(app, 
     browser: :chrome,
     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-        "chromeOptions" => {"args" => %w{--log-level=3 --disable-web-security --no-sandbox --start-maximized --disable-print-preview}}))
+        "chromeOptions" => {"args" => %w{ --log-level=3 --disable-web-security --no-sandbox --start-maximized --disable-print-preview --disable-dev-shm-usage}}))        
     end
 Capybara.default_driver = :chrome
-#Capybara.default_driver = :selenium
 #Capybara.page.current_window.resize_to(1366,740)
 Capybara.default_max_wait_time = 25 
 end 
 
+=end
+#======Executa os testes no chrome HEADLESS MODE=========  
 
-#======Executa os testes em HEADLESS MODE=========  
 
-=begin 
 begin Capybara.register_driver :selenium_chrome_headless do |app|
   Capybara::Selenium::Driver.new(app, 
   browser: :chrome,
   :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-      "chromeOptions" => {"args" => %w{--headless --window-size=2000,5000 --log-level=3 --disable-web-security --no-sandbox --disable-print-preview}}))
-  end
+      "chromeOptions" => {"args" => %w{ --headless --disable-gpu  --window-size=2000,4000 --log-level=3 --disable-web-security --no-sandbox --disable-print-preview --disable-dev-shm-usage}}))  
+    end
   Capybara.default_driver = :selenium_chrome_headless
-  Capybara.default_max_wait_time = 45
+  Capybara.default_max_wait_time = 25
 end
+
+
+#======Executa os testes no firefox=========  
+
+=begin 
+Capybara.default_driver = :selenium
+#Capybara.page.current_window.resize_to(1600,900)
+Capybara.page.current_window.maximize
+Capybara.default_max_wait_time = 25 
+=end
+
+
+#======Executa os testes em firefox HEADLESS MODE=========  
+
+=begin 
+Capybara.register_driver :firefox_headless do |app|
+    options = ::Selenium::WebDriver::Firefox::Options.new
+    options.args << '--headless '
+  
+    Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+  end
+Capybara.default_driver = :firefox_headless
+Capybara.default_max_wait_time = 25 
 =end
