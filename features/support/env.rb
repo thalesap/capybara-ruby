ebbd8a16302ff6c5906b22c4ba00e919920b6a1d
Thales Andrade Pereira <thales_ap@hotmail.com>
2019-04-05 15:01:07 -0300

    CM - Melhoria codigo env

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,40 +16,27 @@ Capybara.app_host = 'https://automacaocombatista.herokuapp.com/'
 ENVIRONMENT_TYPE = ENV['ENVIRONMENT_TYPE']
 HEADLESS = ENV['HEADLESS']
 
-if HEADLESS.eql?('no_headless') 
-  begin Capybara.register_driver :chrome do |app|
-
+begin Capybara.register_driver :chrome do |app|
+  if HEADLESS.eql?('no_headless') 
     Capybara::Selenium::Driver.new(app, browser: :chrome,
     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-      "chromeOptions" => {"args" => %w{ --start-maximized --log-level=3 --disable-web-security --no-sandbox --disable-print-preview --disable-dev-shm-usage}}))  
-    end
+    "chromeOptions" => {"args" => %w{ --start-maximized --log-level=3 --disable-web-security --no-sandbox --disable-print-preview --disable-dev-shm-usage}}))  
 
-    Capybara.configure do |config|
-      config.app_host = CONFIG['url_default']
-    end
+#======Executa os testes no chrome HEADLESS MODE=========  
 
-    Capybara.default_driver = :chrome
-    Capybara.default_max_wait_time = 30 
-  end 
-
-  #======Executa os testes no chrome HEADLESS MODE=========  
-
-elsif HEADLESS.eql?('headless')
-  begin Capybara.register_driver :selenium_chrome_headless do |app|
+  elsif HEADLESS.eql?('headless')
     ENV['DOWNLOADPATH'] = '/var/jenkins_home/workspace/TestesAutomatizadosCPA/testes-automatizados-cpa/cucumber-capybara-cpa'
-
+  
     Capybara::Selenium::Driver.new(app, browser: :chrome,
     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-      "chromeOptions" => {"args" => %w{ --headless --disable-gpu  --window-size=2000,2500 --log-level=3 --disable-web-security --no-sandbox --disable-print-preview --disable-dev-shm-usage}}))  
-    end
-
-    Capybara.configure do |config|
-      config.app_host = CONFIG['url_default']
-    end
-
-    Capybara.default_driver = :selenium_chrome_headless
-    Capybara.default_max_wait_time = 30
+    "chromeOptions" => {"args" => %w{ --headless --disable-gpu  --window-size=2000,2500 --log-level=3 --disable-web-security --no-sandbox --disable-print-preview --disable-dev-shm-usage}}))  
+    
   end
+end
+  Capybara.configure do |config|
+    config.default_driver = :chrome
+    config.default_max_wait_time = 30 
+  end 
 end
 
 
